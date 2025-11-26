cb0b6e1861a25f5bf00465cfbe18e83ae5bebcfa
Jeffrey Urban <Jeffrey@JeffreyUrban.com>
2025-11-26 17:09:53 -0500

    Add uniqseq formula v0.1.1
    - Stream-based deduplication for repeating sequences
    - Python 3.14 with complete dependency tree
    - Includes: typer, rich, click, shellingham, markdown-it-py, mdurl, pygments
    - Comprehensive tests: version, functionality, help output

---
+++ Formula/uniqseq.rb
@@ -0,0 +1,65 @@
+class Uniqseq < Formula
+  include Language::Python::Virtualenv
+
+  desc "Stream-based deduplication for repeating sequences"
+  homepage "https://github.com/JeffreyUrban/uniqseq"
+  url "https://files.pythonhosted.org/packages/9e/23/f4dc31e90d0a458a9d2ec4b236c6440f951864ff389307600ae0b804644e/uniqseq-0.1.1.tar.gz"
+  sha256 "fcb8d59ee69c918a3634b3a425ae47e2184c6587d132f6f944bd68c443f80e80"
+  license "MIT"
+
+  depends_on "python@3.14"
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/3d/fa/656b739db8587d7b5dfa22e22ed02566950fbfbcdc20311993483657a5c0/click-8.3.1.tar.gz"
+    sha256 "12ff4785d337a1bb490bb7e9c2b1ee5da3112e94a8622f26a6c77f5d2fc6842a"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/5b/f5/4ec618ed16cc4f8fb3b701563655a69816155e79e24a17b651541804721d/markdown_it_py-4.0.0.tar.gz"
+    sha256 "cb0a2b4aa34f932c007117b194e945bd74e0ec24133ceb5bac59009cda1cb9f3"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/b0/77/a5b8c569bf593b0140bde72ea885a803b82086995367bf2037de0159d924/pygments-2.19.2.tar.gz"
+    sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/fb/d2/8920e102050a0de7bfabeb4c4614a49248cf8d5d7a8d01885fbb24dc767a/rich-14.2.0.tar.gz"
+    sha256 "73ff50c7c0c1c77c8243079283f4edb376f0f6442433aecb8ce7e6d0b92d1fe4"
+  end
+
+  resource "shellingham" do
+    url "https://files.pythonhosted.org/packages/58/15/8b3609fd3830ef7b27b655beb4b4e9c62313a4e8da8c676e142cc210d58e/shellingham-1.5.4.tar.gz"
+    sha256 "8dbca0739d487e5bd35ab3ca4b36e11c4078f3a234bfce294b0a0291363404de"
+  end
+
+  resource "typer" do
+    url "https://files.pythonhosted.org/packages/8f/28/7c85c8032b91dbe79725b6f17d2fffc595dff06a35c7a30a37bef73a1ab4/typer-0.20.0.tar.gz"
+    sha256 "1aaf6494031793e4876fb0bacfa6a912b551cf43c1e63c800df8b1a866720c37"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    # Test version output
+    output = shell_output("#{bin}/uniqseq --version")
+    assert_match "uniqseq version 0.1.1", output
+
+    # Test basic functionality
+    (testpath/"test.txt").write("line1\nline2\nline3\nline1\nline2\nline3\n")
+    output = shell_output("#{bin}/uniqseq --window-size 3 --quiet #{testpath}/test.txt")
+    assert_equal "line1\nline2\nline3\n", output
+
+    # Test help output
+    output = shell_output("#{bin}/uniqseq --help")
+    assert_match "Remove duplicate record sequences", output
+  end
+end
